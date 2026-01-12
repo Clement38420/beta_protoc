84beab7238244b6efc3cfe8b98d42a255e765c1e
Clément Charbonnel <c.charbo38@gmail.com>
2026-01-13 00:24:45 +0100

    feat(protocol): implement serialization and deserialization functions
    Add functions to convert primitive data types to and from a byte buffer. These functions will be reused in all generated C files.

---
+++ protoc_common_code/C/beta_protoc.c
@@ -0,0 +1,201 @@
+#include "beta_protoc.h"
+
+#include <string.h>
+
+static beta_protoc_err_t _write_unsigned(uint64_t data, size_t size, uint8_t **buff, size_t *buff_len) {
+    if (*buff_len < size) return BETA_PROTOC_ERR_BUFFER_TOO_SMALL;
+
+    for (size_t i = 0; i < size; i++) {
+        **buff = (uint8_t)(data & 0xFF);
+        (*buff)++;
+        data >>= 8;
+    }
+    *buff_len -= size;
+    return BETA_PROTOC_SUCCESS;
+}
+
+beta_protoc_err_t int8_to_buff(int8_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint8_t)data, 1, buff, buff_len);
+}
+
+beta_protoc_err_t int16_to_buff(int16_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint16_t)data, 2, buff, buff_len);
+}
+
+beta_protoc_err_t int32_to_buff(int32_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint32_t)data, 4, buff, buff_len);
+}
+
+beta_protoc_err_t int64_to_buff(int64_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint64_t)data, 8, buff, buff_len);
+}
+
+beta_protoc_err_t uint8_to_buff(uint8_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned(data, 1, buff, buff_len);
+}
+
+beta_protoc_err_t uint16_to_buff(uint16_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned(data, 2, buff, buff_len);
+}
+
+beta_protoc_err_t uint32_to_buff(uint32_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned(data, 4, buff, buff_len);
+}
+
+beta_protoc_err_t uint64_to_buff(uint64_t data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned(data, 8, buff, buff_len);
+}
+
+beta_protoc_err_t float_to_buff(float data, uint8_t **buff, size_t *buff_len) {
+    union { float f; uint32_t u; } converter;
+    converter.f = data;
+
+    return _write_unsigned(converter.u, 4, buff, buff_len);
+}
+
+beta_protoc_err_t double_to_buff(double data, uint8_t **buff, size_t *buff_len) {
+    union { double f; uint64_t u; } converter;
+    converter.f = data;
+
+    return _write_unsigned(converter.u, 8, buff, buff_len);
+}
+
+beta_protoc_err_t string_to_buff(char *data, size_t data_len, uint8_t **buff, size_t *buff_len) {
+    if (buff == NULL || *buff == NULL || buff_len == NULL) {
+        return BETA_PROTOC_ERR_INVALID_ARGS;
+    }
+
+    if (*buff_len < data_len) return BETA_PROTOC_ERR_BUFFER_TOO_SMALL;
+
+    memcpy(*buff, data, data_len);
+    *buff += data_len;
+    *buff_len -= data_len;
+
+    return BETA_PROTOC_SUCCESS;
+}
+
+beta_protoc_err_t char_to_buff(char data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint8_t)data, 1, buff, buff_len);
+}
+
+beta_protoc_err_t bool_to_buff(bool data, uint8_t **buff, size_t *buff_len) {
+    return _write_unsigned((uint8_t)data, 1, buff, buff_len);
+}
+
+static beta_protoc_err_t _read_unsigned(uint64_t *data, size_t size, uint8_t **buff, size_t *buff_len) {
+    if (buff == NULL || *buff == NULL || buff_len == NULL || data == NULL) {
+        return BETA_PROTOC_ERR_INVALID_ARGS;
+    }
+    if (*buff_len < size) return BETA_PROTOC_ERR_BUFFER_TOO_SMALL;
+
+    *data = 0;
+    for (size_t i = 0; i < size; i++) {
+        *data |= ((uint64_t)(**buff) << (8 * i));
+        (*buff)++;
+    }
+    *buff_len -= size;
+    return BETA_PROTOC_SUCCESS;
+}
+
+beta_protoc_err_t int8_from_buff(int8_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 1, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (int8_t)temp;
+    return err;
+}
+
+beta_protoc_err_t int16_from_buff(int16_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 2, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (int16_t)temp;
+    return err;
+}
+
+beta_protoc_err_t int32_from_buff(int32_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 4, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (int32_t)temp;
+    return err;
+}
+
+beta_protoc_err_t int64_from_buff(int64_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 8, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (int64_t)temp;
+    return err;
+}
+
+beta_protoc_err_t uint8_from_buff(uint8_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 1, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (uint8_t)temp;
+    return err;
+}
+
+beta_protoc_err_t uint16_from_buff(uint16_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 2, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (uint16_t)temp;
+    return err;
+}
+
+beta_protoc_err_t uint32_from_buff(uint32_t *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 4, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (uint32_t)temp;
+    return err;
+}
+
+beta_protoc_err_t uint64_from_buff(uint64_t *data, uint8_t **buff, size_t *buff_len) {
+    return _read_unsigned(data, 8, buff, buff_len);
+}
+
+beta_protoc_err_t float_from_buff(float *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 4, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) {
+        union { uint32_t u; float f; } converter;
+        converter.u = (uint32_t)temp;
+        *data = converter.f;
+    }
+    return err;
+}
+
+beta_protoc_err_t double_from_buff(double *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 8, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) {
+        union { uint64_t u; double f; } converter;
+        converter.u = temp;
+        *data = converter.f;
+    }
+    return err;
+}
+
+beta_protoc_err_t string_from_buff(char *data, size_t data_len, uint8_t **buff, size_t *buff_len) {
+    if (buff == NULL || *buff == NULL || buff_len == NULL || data == NULL) {
+        return BETA_PROTOC_ERR_INVALID_ARGS;
+    }
+
+    if (*buff_len < data_len) return BETA_PROTOC_ERR_BUFFER_TOO_SMALL;
+
+    memcpy(data, *buff, data_len);
+    *buff += data_len;
+    *buff_len -= data_len;
+
+    return BETA_PROTOC_SUCCESS;
+}
+
+beta_protoc_err_t char_from_buff(char *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 1, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (char)temp;
+    return err;
+}
+
+beta_protoc_err_t bool_from_buff(bool *data, uint8_t **buff, size_t *buff_len) {
+    uint64_t temp;
+    beta_protoc_err_t err = _read_unsigned(&temp, 1, buff, buff_len);
+    if (err == BETA_PROTOC_SUCCESS) *data = (bool)temp;
+    return err;
+}
---
+++ protoc_common_code/C/beta_protoc.h
@@ -0,0 +1,51 @@
+#ifndef BETA_PROTOC_H
+#define BETA_PROTOC_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define PROTOCOL_VERSION 1
+#define MESSAGE_HEADER_SIZE 3 // 1 byte for protocol version, 1 byte for message ID, 1 byte for message length
+
+typedef enum {
+    BETA_PROTOC_SUCCESS = 0,               // Operation successful
+    BETA_PROTOC_ERR_INVALID_ARGS = -10,     // NULL pointers passed as parameters
+    BETA_PROTOC_ERR_BUFFER_TOO_SMALL = -11, // Output or work buffer too small
+} beta_protoc_err_t;
+
+beta_protoc_err_t int8_to_buff(int8_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int16_to_buff(int16_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int32_to_buff(int32_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int64_to_buff(int64_t data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t uint8_to_buff(uint8_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint16_to_buff(uint16_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint32_to_buff(uint32_t data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint64_to_buff(uint64_t data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t float_to_buff(float data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t double_to_buff(double data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t string_to_buff(char *data, size_t data_len, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t char_to_buff(char data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t bool_to_buff(bool data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t int8_from_buff(int8_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int16_from_buff(int16_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int32_from_buff(int32_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t int64_from_buff(int64_t *data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t uint8_from_buff(uint8_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint16_from_buff(uint16_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint32_from_buff(uint32_t *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t uint64_from_buff(uint64_t *data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t float_from_buff(float *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t double_from_buff(double *data, uint8_t **buff, size_t *buff_len);
+
+beta_protoc_err_t bool_from_buff(bool *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t char_from_buff(char *data, uint8_t **buff, size_t *buff_len);
+beta_protoc_err_t string_from_buff(char *data, size_t data_len, uint8_t **buff, size_t *buff_len);
+
+#endif //BETA_PROTOC_H
